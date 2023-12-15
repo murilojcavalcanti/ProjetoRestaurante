5f020f7f7a4cccdc684cdf0fcefb134315276215
AmandaaBastos <amandabastoscorreia@gmail.com>
2023-12-15 17:16:23 -0300

    Classe conta adicionada

---
+++ Conta.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class Conta
+{
+    private List<Pedido> pedidos = new List<Pedido>();
+    public bool Pago { get; private set; }
+
+    
+    public void AdicionarPedido(Pedido pedido)
+    {
+        pedidos.Add(pedido);
+    }
+
+    public decimal CalcularTotal()
+    {
+        decimal total = 0;
+
+        foreach (var pedido in pedidos)
+        {
+            total += pedido.CalcularTotal();
+        }
+
+        return total;
+    }
+
+    public void ExibirDetalhes()
+    {
+        Console.WriteLine("Detalhes da Conta:");
+
+        foreach (var pedido in pedidos)
+        {
+            pedido.ExibirItens();
+            Console.WriteLine("-------------------------");
+        }
+
+        Console.WriteLine($"Total da Conta: R$ {CalcularTotal():F2}");
+    }
+
+    public void Pagar()
+    {
+        Pago = true;
+        Console.WriteLine("Pedido foi pago.");
+    }
+
+}
+
