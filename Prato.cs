647ed7cec2782b0f72f646fc6941cedcf97486e4
Edvaldo Oliveira <edvaldooliveira@alu.ufc.br>
2023-12-15 17:08:46 -0300

    criada a classe Prato

---
+++ Prato.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoRestauranteUsuario
+{
+    public abstract class Alimento
+    {
+        private double preco { get; set; }
+        private string nome { get; set; }
+        private string descricao { get; set; }
+        
+        public Alimento(string nome, double preco, string descricao)
+        {
+            this.nome = nome;
+            this.preco = preco;
+            this.descricao = descricao;
+        }
+
+        public obterInformações
+    }
+}
