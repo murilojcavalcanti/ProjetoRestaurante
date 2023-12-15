54c14a3846c254da0ca8cdc6d69df28693c80b83
Maria Tauany Santos Feitosa <tauanysanttos13@gmail.com>
2023-12-15 16:45:26 -0300

    add: Console menu

--- Program.cs
+++ Program.cs
@@ -2,9 +2,12 @@
 {
     public class Program
     {
-        public static void Main(string[] args) 
+        public static void Main(string[] args)
         {
             Console.WriteLine("Teste");
+            string[] opcoes = { "testando opcoes", "testando opcoes 2" };
+            var escolha = new ConsoleMenuUsuario(opcoes);
+            Console.WriteLine(escolha.ShowMenu());
         }
     }
 }
